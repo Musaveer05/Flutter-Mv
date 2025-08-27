aac96147cc51abb0ebde259eded0aa43febba59d
Musaveer <musaveer05@gmail.com>
2025-08-27 14:06:04 +0530

    Added Web native display specifically addKVDataChangeListener() method

--- android/app/src/main/kotlin/com/example/clevertap/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/clevertap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.clevertap
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
-
+class MainActivity : FlutterFragmentActivity() {
+    // FlutterFragmentActivity supports Header & Footer In-App Notification Templates
+}
